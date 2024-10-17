c3fbfb38c1f57b2aac851590e5bbde976cfac300
Álvaro Fernández Amaro <alvaroofernaandez@gmail.com>
2024-10-17 12:57:07 +0200

    postgresql, django configurado

--- apidjango/apidjango/settings.py
+++ apidjango/apidjango/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'apidjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django-api',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
